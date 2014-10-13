9f01be0a5b33f6e7739f646bd9b8942b7ca29af2
Martin Leinberger <martin.leinberger@googlemail.com>
2014-10-13 16:56:37 +0200

    Implemented gather instances

--- LiteqLoD/RdfResource.fs
+++ LiteqLoD/RdfResource.fs
@@ -3,8 +3,11 @@
 open System
 open VDS.RDF
 open VDS.RDF.Parsing
+open Schema
 
-let loadResource (uri : Uri) = 
+type Pattern = string*string*string
+
+let loadResource (uri : System.Uri) = 
     let graph = new Graph()
     UriLoader.Load(graph, uri)
     graph
@@ -14,14 +17,14 @@ type RdfResource(graph : Graph) =
     class
         member __.Graph = graph
         member __.Item 
-            with get (uri : Uri) = graph.GetTriplesWithPredicate uri |> Seq.map (fun x -> x.Object)
+            with get (uri : System.Uri) = graph.GetTriplesWithPredicate uri |> Seq.map (fun x -> x.Object)
     end
 
-let filterPropertyType (resource : RdfResource) (propertyName:Uri,typeName:Uri) = 
+let filterPropertyType (resource : RdfResource) (propertyName:System.Uri, typeName:System.Uri) = 
     resource.Graph.GetTriplesWithPredicate propertyName
     |> Seq.map (fun triple -> 
            match triple.Object with
-           | :? UriNode -> (loadResource (triple.Object :?> UriNode).Uri).GetTriplesWithPredicate(Uri "rdf:type")
+           | :? UriNode -> (loadResource (triple.Object :?> UriNode).Uri).GetTriplesWithPredicate(System.Uri "rdf:type")
            | :? LiteralNode -> failwith "Everythings fucked..."
            | _ -> failwith "Even more fucked")
     |> Seq.concat
@@ -30,15 +33,22 @@ let filterPropertyType (resource : RdfResource) (propertyName:Uri,typeName:Uri) 
 let dummyInstancesFromSchemex : RdfResource list = []
 let dummyRestrictions = ["?yy", "a", "Katze"; "?x", "hatHaustier", "?yy"; "?y", "a", "Hund" ; "?x", "hatAllergie", "?y"; "?x", "a", "Person"]
 
-
-let test = 
-    let propsAndTypes =
-        dummyRestrictions
-        |> List.rev
+let gatherInstances (patterns : Pattern list) =
+    let patterns' = List.rev patterns
+    let initial_type = ``object`` patterns'.[0]
+    let schema = (new DummySchema()) :> SchemaProvider
+    let instances =
+        schema.GetTypeClustersFor initial_type
+        |> List.map(fun typeCluster -> schema.GetAllEQCIn typeCluster)
+        |> List.concat
+        |> List.map(fun equivalenceCluster -> schema.GetAllInstancesIn equivalenceCluster)
+        |> List.concat
+        |> List.map(fun uri -> RdfResource (loadResource <| System.Uri uri))
+    let restrictions = 
+        patterns'
         |> List.tail
         |> Seq.pairwise
-        |> Seq.map(fun ((_,propertyName,_),(_,_,typeName)) -> Uri propertyName, Uri typeName)
-    dummyInstancesFromSchemex
-    |> Seq.filter (fun instance -> propsAndTypes |> Seq.forall (filterPropertyType instance) )
-
+        |> Seq.map(fun ((_,propertyName,_),(_,_,typeName)) -> System.Uri propertyName, System.Uri typeName)
+    instances
+    |> Seq.filter (fun instance -> restrictions |> Seq.forall (filterPropertyType instance))
 
